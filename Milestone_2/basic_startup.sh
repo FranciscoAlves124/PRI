072200082a2ab216be133d008c1ab1e2b58d2dad
up202108686 <up202108686@up.pt>
2025-11-13 15:19:07 +0000

    Refactored startup scripts: removed old scripts and consolidated functionality into a new startup.sh. Two running modes: ./startup.sh [basic|intermediate], the first being in schemaless mode.

--- Milestone_2/basic_startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script expects a container started with the following command.
-docker run -p 8983:8983 --name initial_solr -v ${PWD}:/data -d solr:9 solr-precreate media
-
-# Populate collection using mapped path inside container.
-docker exec initial_solr solr post -c media /data/final_data_solr/movies_series.json
