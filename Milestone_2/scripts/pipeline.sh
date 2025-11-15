b710dc194bdc85c563000eb621895391da350e6d
FranciscoAlves124 <up202006281@up.pt>
2025-11-15 19:48:28 +0000

    Queries and results. Work in Progress

--- Milestone_2/scripts/pipeline.sh
+++ Milestone_2/scripts/pipeline.sh
@@ -4,14 +4,14 @@
 #./scripts/qrels2trec.py --qrels config/qrels > results/trec_qrels.txt
 
 # query solr and convert results to trec format
-python scripts/query_solr.py --queries config/queries --uri http://localhost:8983/solr --collection media
+python scripts/query_solr.py --queries config/queries/query1 --uri http://localhost:8983/solr --collection media
 ./scripts/solr2trec.py > results/trec_results.txt
 
 # run evaluation pipeline
-./trec_eval/trec_eval \
-    -q -m all_trec \
-    results/trec_qrels.txt results/trec_results.txt | ./scripts/plot_pr.py
+#./trec_eval/trec_eval \
+#    -q -m all_trec \
+#    results/trec_qrels.txt results/trec_results.txt | ./scripts/plot_pr.py
 
 # cleanup
 #rm results/trec_qrels.txt
-rm results/trec_results.txt
+#rm results/trec_results.txt
